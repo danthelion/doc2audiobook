2b14304cb940be07c43c907a9eded67e0cec9f24
danthelion <danivgy@gmail.com>
2018-04-01 12:26:21 +0200

    Fix docs, add logging.

--- doc2audiobook.py
+++ doc2audiobook.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import textract
@@ -5,8 +6,6 @@ from google.cloud import texttospeech
 
 from lib.logging import get_module_logger
 from lib.tts_utils import list_voices, text_to_mp3, collect_input_files
-
-import argparse
 
 logger = get_module_logger(__name__)
 
@@ -43,9 +42,9 @@ def process_input_files(input_directory_path: Path,
     input_files = collect_input_files(input_directory_path=input_directory_path)
 
     for input_file in input_files:
-        logger.info(f'Processing input file {input_file}')
+        logger.info(f'Processing input file `{input_file}`')
         output_file = output_directory_path / (input_file.stem + '.mp3')
-        logger.info(f'Target output file is: {output_file}')
+        logger.info(f'Target output file is: `{output_file}`')
 
         text_to_translate = textract.process(str(input_file))
 
@@ -53,9 +52,11 @@ def process_input_files(input_directory_path: Path,
             client=client,
             voice=voice,
             audio_config=audio_config,
-            text=text_to_translate,
+            text=text_to_translate[:5000],
             output_file_path=output_file
         )
+
+        logger.info(f'Processing done for input file `{input_file}`')
 
 
 def main():
@@ -67,14 +68,13 @@ def main():
         print(f'Available voices\n{available_voices}\n')
         return
 
-    # use_voice = input('Which voice to use?')
     use_voice = args.voice
     if use_voice not in available_voices:
         exit('Invalid voice! Use -list-voices to see all available options.')
 
     use_language = '-'.join(use_voice.split('-')[0:2])
 
-    logger.info(f'Using voice {use_voice} in language {use_language}')
+    logger.info(f'Using voice `{use_voice}` in language `{use_language}`')
 
     voice = texttospeech.types.VoiceSelectionParams(language_code=use_language, name=use_voice)
     audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3)
@@ -83,6 +83,9 @@ def main():
         input_directory_path=Path('/data/input'), output_directory_path=Path('/data/output'),
         client=client, voice=voice, audio_config=audio_config
     )
+
+    logger.info('Done!')
+    logger.info('Failures have been saved next to their respective output files.')
 
 
 if __name__ == '__main__':
--- lib/tts_utils.py
+++ lib/tts_utils.py
@@ -1,5 +1,7 @@
-from typing import List
+import json
+import traceback
 from pathlib import Path
+from typing import List
 
 from google.cloud import texttospeech
 
@@ -8,12 +10,12 @@ from lib.logging import get_module_logger
 logger = get_module_logger(__name__)
 
 
-def collect_input_files(input_directory_path: Path):
+def collect_input_files(input_directory_path: Path) -> List(Path):
     """
     Grab every file inside the input directory.
 
     :param input_directory_path: Full path to the input directory.
-    :return:
+    :return: List of full path of every file inside the input folder.
     """
     return input_directory_path.glob('**/*')
 
@@ -26,29 +28,45 @@ def text_to_mp3(client: texttospeech.TextToSpeechClient,
     """
     Convert a string into voice and save it in an .mp3 file.
 
-    :param client:
-    :param voice:
-    :param audio_config:
-    :param text:
-    :param output_file_path:
-    :return:
+    :param client: TextToSpeechClient instance.
+    :param voice: VoiceSelectionParams instance.
+    :param audio_config: AudioConfig instance.
+    :param text: String to synthesise.
+    :param output_file_path: Full path to the output .mp3 file.
+    :return: None
     """
     lines = text.splitlines()
 
-    logger.info(f'Consuming {len(lines)} lines ...')
+    logger.info(f'Synthesising {len(lines)} lines ...')
 
-    with open(output_file_path, 'wb') as out:
+    output_file_log = output_file_path.parent / (output_file_path.stem + '_log.json')
+
+    with open(output_file_path, 'wb') as output_file:
         for (i, text_chunk) in enumerate(lines):
             # skip empty lines
             if len(text_chunk) > 0:
-                logger.info(f'Synthesising input for chunk {i}, size: {len(text_chunk)}')
                 input_text = texttospeech.types.SynthesisInput(text=text_chunk)
-                logger.info(f'Synthesising speech for chunk {i}, size: {len(text_chunk)}')
-                response = client.synthesize_speech(input_text, voice, audio_config)
-                # this is fine because mp3s can be concatenated naively and still work
-                logger.info(f'Writing Audio content to {output_file_path} ...')
-                out.write(response.audio_content)
-                logger.info(f'Audio content written to {output_file_path}!')
+                try:
+                    logger.info(f'Synthesising speech for chunk `{i}`, size: `{len(text_chunk)}`')
+                    response = client.synthesize_speech(input_text, voice, audio_config)
+                except Exception as e:
+                    # If a line could not be synthesised properly, return it along with the error message
+                    # It is possible that textract could not extract the text properly.
+                    logger.error(f'Speech synthesising failed! Chunk text: `{input_text}`\nError: {e}\n')
+                    _error_log = {
+                        'chunk_number': i,
+                        'chunk_length': len(text_chunk),
+                        'chunk_text': str(text_chunk),
+                        'Error message': traceback.format_exc()
+                    }
+                    with open(f'{output_file_log}', 'w') as log_out:
+                        json.dump(_error_log, log_out)
+                    continue
+                output_file.write(response.audio_content)
+                logger.info(f'Audio content written to `{output_file_path}`!')
+
+        logger.info(f'Output saved to `{output_file_path}`')
+        logger.info(f'logs at `{output_file_log}`')
 
 
 def list_voices(client) -> List[str]:
