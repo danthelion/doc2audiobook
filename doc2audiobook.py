186e1d4212f2417176a23a22bfcfbfac31ce435c
danthelion <danivgy@gmail.com>
2018-04-01 10:31:51 +0200

    Add better CLI argument handling.

--- doc2audiobook.py
+++ doc2audiobook.py
@@ -4,34 +4,86 @@ import textract
 from google.cloud import texttospeech
 
 from lib.logging import get_module_logger
-from lib.tts_utils import list_voices, text_to_mp3
+from lib.tts_utils import list_voices, text_to_mp3, collect_input_files
+
+import argparse
 
 logger = get_module_logger(__name__)
 
-client = texttospeech.TextToSpeechClient()
 
-available_voices = list_voices(client=client)
+def parse_arguments():
+    """
+    Parse command line arguments.
 
-logger.info(available_voices)
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-list-voices', help='List available voices.', action='store_true')
+    parser.add_argument('--voice', type=str, help='Voice to use for synthesis. Use -list-voices to see options.')
 
-# use_voice = input('Which voice to use?')
-use_voice = 'en-US-Wavenet-F'
-use_language = '-'.join(use_voice.split('-')[0:2])
+    return parser.parse_args()
 
-logger.info(f'Using voice {use_voice} in language {use_language}')
 
-voice = texttospeech.types.VoiceSelectionParams(language_code=use_language, name=use_voice)
-audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+def process_input_files(input_directory_path: Path,
+                        output_directory_path: Path,
+                        client: texttospeech.TextToSpeechClient,
+                        voice: texttospeech.types.VoiceSelectionParams,
+                        audio_config: texttospeech.types.AudioConfig
+                        ) -> None:
+    """
+    Process every file inside `input_directory_path` and save results in `output_directory_path`.
 
-input_path = list(Path('/data/input').glob('**/*'))[0]
-logger.info(f'Processing file {input_path}')
+    :param input_directory_path: Full path to the input directory.
+    :param output_directory_path: Full path to the output directory.
+    :param client: TextToSpeechClient instance.
+    :param voice: VoiceSelectionParams instance.
+    :param audio_config: AudioConfig instance.
+    :return: None
+    """
+    input_files = collect_input_files(input_directory_path=input_directory_path)
 
-text_to_translate = textract.process(input_path)
+    for input_file in input_files:
+        logger.info(f'Processing input file {input_file}')
+        output_file = output_directory_path / (input_file.stem + '.mp3')
+        logger.info(f'Target output file is: {output_file}')
 
-text_to_mp3(
-    client=client,
-    voice=voice,
-    audio_config=audio_config,
-    text=text_to_translate,
-    file_dest=f'/data/output/output.mp3'
-)
+        text_to_translate = textract.process(str(input_file))
+
+        text_to_mp3(
+            client=client,
+            voice=voice,
+            audio_config=audio_config,
+            text=text_to_translate,
+            output_file_path=output_file
+        )
+
+
+def main():
+    client = texttospeech.TextToSpeechClient()
+    available_voices = list_voices(client=client)
+
+    args = parse_arguments()
+    if args.list_voices:
+        print(f'Available voices\n{available_voices}\n')
+        return
+
+    # use_voice = input('Which voice to use?')
+    use_voice = args.voice
+    if use_voice not in available_voices:
+        exit('Invalid voice! Use -list-voices to see all available options.')
+
+    use_language = '-'.join(use_voice.split('-')[0:2])
+
+    logger.info(f'Using voice {use_voice} in language {use_language}')
+
+    voice = texttospeech.types.VoiceSelectionParams(language_code=use_language, name=use_voice)
+    audio_config = texttospeech.types.AudioConfig(audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    process_input_files(
+        input_directory_path=Path('/data/input'), output_directory_path=Path('/data/output'),
+        client=client, voice=voice, audio_config=audio_config
+    )
+
+
+if __name__ == '__main__':
+    main()
--- lib/tts_utils.py
+++ lib/tts_utils.py
@@ -1,4 +1,5 @@
 from typing import List
+from pathlib import Path
 
 from google.cloud import texttospeech
 
@@ -7,9 +8,36 @@ from lib.logging import get_module_logger
 logger = get_module_logger(__name__)
 
 
-def text_to_mp3(client, voice, audio_config, text, file_dest):
+def collect_input_files(input_directory_path: Path):
+    """
+    Grab every file inside the input directory.
+
+    :param input_directory_path: Full path to the input directory.
+    :return:
+    """
+    return input_directory_path.glob('**/*')
+
+
+def text_to_mp3(client: texttospeech.TextToSpeechClient,
+                voice: texttospeech.types.VoiceSelectionParams,
+                audio_config: texttospeech.types.AudioConfig,
+                text: str,
+                output_file_path: Path) -> None:
+    """
+    Convert a string into voice and save it in an .mp3 file.
+
+    :param client:
+    :param voice:
+    :param audio_config:
+    :param text:
+    :param output_file_path:
+    :return:
+    """
     lines = text.splitlines()
-    with open(file_dest, 'wb') as out:
+
+    logger.info(f'Consuming {len(lines)} lines ...')
+
+    with open(output_file_path, 'wb') as out:
         for (i, text_chunk) in enumerate(lines):
             # skip empty lines
             if len(text_chunk) > 0:
@@ -18,8 +46,9 @@ def text_to_mp3(client, voice, audio_config, text, file_dest):
                 logger.info(f'Synthesising speech for chunk {i}, size: {len(text_chunk)}')
                 response = client.synthesize_speech(input_text, voice, audio_config)
                 # this is fine because mp3s can be concatenated naively and still work
+                logger.info(f'Writing Audio content to {output_file_path} ...')
                 out.write(response.audio_content)
-                logger.info(f'Audio content written to file {file_dest}')
+                logger.info(f'Audio content written to {output_file_path}!')
 
 
 def list_voices(client) -> List[str]:
